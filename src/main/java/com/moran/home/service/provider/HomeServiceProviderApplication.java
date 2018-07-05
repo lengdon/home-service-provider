2bf09048c5586349020bd598470ff91d3e368a61
Rahul <raahool_16@rediffmail.com>
2018-07-05 15:10:42 +0530

    Initial commit

---
+++ src/main/java/com/moran/home/service/provider/HomeServiceProviderApplication.java
@@ -0,0 +1,12 @@
+package com.moran.home.service.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeServiceProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeServiceProviderApplication.class, args);
+	}
+}
